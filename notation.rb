84ff029d9e8da2bb87d6ec5a91d2378cbec06a6f
catherine-ne <73527275+catherine-ne@users.noreply.github.com>
2022-11-22 22:58:09 +0200

    polish notation

---
+++ notation.rb
@@ -0,0 +1,61 @@
+#Функция перевода в польскую нотацию
+def opz(iStr, stack)
+  priority = Hash["(" => 0, "+" => 1, "-" => 1, "*" => 2, "/" => 2, "^" => 3]
+  case iStr
+  when /^\s*([^\+\-\*\/\(\)\^\s]+)\s*(.*)/ then $1 + " " + opz($2, stack)
+  when /^\s*([\+\-\*\/\^])\s*(.*)/
+    if (stack.empty? or priority[stack.last] < priority[$1]) then
+      opz($2, stack.push($1))
+    else
+      stack.pop + " " + opz(iStr, stack)
+    end
+  when /^\s*\(\s*(.*)/ then opz($1, stack.push("("))
+  when /^\s*\)\s*(.*)/
+    if stack.empty? then
+      raise "Error: Excess of closing brackets."
+    elsif priority[head = stack.pop] > 0 then
+      head + " " + opz(iStr, stack)
+    else
+      opz($1, stack)
+    end
+  else
+    if stack.empty? then
+      ""
+    elsif priority[stack.last] > 0 then
+      stack.pop + " " + opz(iStr, stack)
+    else
+      raise "Error: Excess of opening brackets."
+    end
+  end
+end
+
+#Хєш для вычислений
+OPERATIONS = {
+  "+" => lambda { |a, b| a + b },
+  "-" => lambda { |a, b| a - b },
+  "*" => lambda { |a, b| a * b },
+  "/" => lambda { |a, b| a / b }
+}
+
+#Функция для вычисления выражения
+def eval_polish(expression)
+  elements = expression.split
+  stack = []
+  elements.each { |element|
+    if OPERATIONS.keys.include? element
+      b = stack.pop
+      a = stack.pop
+      stack.push OPERATIONS[element].call a, b
+    else
+      stack.push element.to_i
+    end
+  }
+  stack.pop
+end
+
+puts "Введите выражение: "
+a = gets
+polnot = opz(a, [])
+puts "Запись в обратной польской нотации: " + polnot.to_s
+puts "Результат вычислений: " + eval_polish(polnot).to_s
+
